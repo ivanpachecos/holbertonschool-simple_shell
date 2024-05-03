2b9e9fcda8e4b0da2fe0b03cfafe88b7dcad56e5
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-03 00:18:55 -0500

    add styles betty promt

--- promt.c
+++ promt.c
@@ -1,10 +1,11 @@
 #include "shell.h"
 
 /**
- * 
- * 
+ * _prompt - Prints the shell prompt
+ *
 */
 void _prompt(void)
-{   
-    write(STDOUT_FILENO, "#cisfun$: ", 11);
+{
+	write(STDOUT_FILENO, "#cisfun$: ", 11);
 }
+
